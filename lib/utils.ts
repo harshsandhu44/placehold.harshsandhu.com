bd48ca51d94a4f8c523f7402e1284b11080ac095
Harsh Sandhu <hsandhu@speareducation.com>
2025-12-19 23:12:54 +0530

    chore: setup shadcn in the project

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function clampInt(v: number, min: number, max: number) {
   return Math.min(Math.max(v, min), max);
 }
